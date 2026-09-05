e8c67def934ed4510622e16c9202ef3521095036
gjgggjgjgjj <davidjarom10@gmail.com>
2026-09-04 18:02:07 -0600

    finished odds simulation of five card stud poker

--- five_card_stud.py
+++ five_card_stud.py
@@ -22,7 +22,7 @@ def fiveDrawn():
     return curHand
 
 #here we need to define if the deck is a winning hand or just in general the ranking on it
-def theCardRank(theHand, winDic):
+def theCardRank(theHand, windic):
     #0-12 is spades
     #13-25 clubs
     #26-38 diamonds
@@ -38,7 +38,7 @@ def theCardRank(theHand, winDic):
     hashrank = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     #bool of hands atm
-    pair = False
+    pair = 0
     threepair = False
     fourpair = False
     straight = False
@@ -52,23 +52,26 @@ def theCardRank(theHand, winDic):
     for i in theHand:
         
         cursuit = i // 13
-        rank = % 13
-        hashsuit.append(cursuit)
-        hashrank.append(rank)
+        rank = i % 13
+        hashsuit[cursuit] += 1
+        hashrank[rank] += 1
 
 
     straightcnt = 0
     hasAce = False
+    hasKing = False
     if hashrank[0] > 0: #to know if we have an ace
         hasAce = True
+    if hashrank[12]:
+        hasKing = True
 
-    for i in range(hashrank):
+    for i in range(len(hashrank)):
         if hashrank[i] == 0:
             straightcnt = 0
             continue
         if hashrank[i] > 1:
             if hashrank[i] == 2:
-                twopair = True
+                pair += 1
             elif hashrank[i] == 3:
                 threepair = True
             elif hashrank[i] == 4:
@@ -79,19 +82,21 @@ def theCardRank(theHand, winDic):
             if i >= 9:  #evaluating the weird straight with ace on top
                 if straightcnt == 4 and hasAce:
                     straight = True
+            if straightcnt == 5:
+                straight = True
 
     # checking if we have a suit
     for i in hashsuit:
-        if i == 4:
+        if i == 5:
             flush = True
 
 
     ##lastly we append to the windic and need to see if we have straight flushes 
     #or if we have full houses
-    if pair and twopair:
+    if pair == 1 and threepair:
         windic["full house"] += 1
         return
-    if flush and straight and hasAce:
+    if flush and straight and hasAce and hasKing:
         windic["royal flush"] += 1
         return
     if flush and straight:
@@ -107,32 +112,48 @@ def theCardRank(theHand, winDic):
         windic["four of a kind"] += 1
         return
     if threepair:
-        windic["three of a kind"]
+        windic["three of a kind"] += 1
         return
-    if twopair:
+    if pair == 2:
         windic["two pair"] += 1
+        return
+    if pair == 1:
+        windic["pair"] += 1
         return
     else:
         windic["high card"] += 1
         return
 
             
+#easier to just get the percent
+def mathPercentage(total,unitAmount):
+    percent = (unitAmount / total) * 100
+    return percent
 
-def printResults(resultsDic):
+#--------------------------------------
+def printResults(totalHands):
+    print("\n\n\n\n==========================================================================================")
+    for i in winDictionary:
+        print(i," : ",winDictionary[i])
+    print("\n\n\n\n")
+    print("The percentage likelyhood of the hands that came out from this experiment are as follows:")
+    print("-------------------------------------------------------------------------------------------")
+    for i in winDictionary:
+        lePercent = mathPercentage(totalHands,winDictionary[i])
+        print(i,"in this experiment occured:","%",lePercent,"amount of time\n")
 
-
-
-
+#just asks user how many times they want to run the sim
 def inputAmountRuns():
-    amount = input("How many times do you wanna ")
-                    
+    amount = int(input("How many times do you wanna run the hands? : ---> "))
+    for i in range(amount):
+        leHand = fiveDrawn()
+        theCardRank(leHand,winDictionary)
 
-
-
-
+    printResults(amount)
 
 
 def main():
+    inputAmountRuns()
 
 
 if __name__ == "__main__":
